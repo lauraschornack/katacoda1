05effc74bc05e2e0a70ae58bff4e09d87cfef995
lauraschornack <laurazinthefer@gmail.com>
2020-02-04 18:13:23 -0600

    add

--- set-up/hello.py
+++ set-up/hello.py
@@ -3,3 +3,10 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+sess = tf.Session()
+hello = tf.comstant("Hello Laura from TensorFlow")
+print (sess.run(hello))
+a = tf.constant(20)
+b = tf.constant(22)
+print('a+B={0}'.format(sess.run(a+b)))
+
