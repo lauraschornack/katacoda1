5387de3099bff849c51e15d5a1859c28ea218776
lauraschornack <laurazinthefer@gmail.com>
2020-03-02 14:25:36 -0600

    cleanup

--- 2-create-ML-model/assets/hello.py
+++ 2-create-ML-model/assets/hello.py
@@ -142,3 +142,11 @@ with tf.Session() as sess:
     #plt.show()
 
     #app.run(debug=True, host='0.0.0.0')
+
+    print("x=")
+    print(train_mq_data_size_norm * train_mq_data_size_std + train_mq_data_size_mean)
+    print("y=)
+    print(sess.run(tf_size_factor) * train_mq_data_size_norm + sess.run(tf_failure_offset)) * train_failure_std + train_failure_mean)
+    print("x/y=")
+    print((train_mq_data_size_norm * train_mq_data_size_std + train_mq_data_size_mean)/(sess.run(tf_size_factor) * train_mq_data_size_norm + sess.run(tf_failure_offset)) * train_failure_std + train_failure_mean))
+    print("if y = mx + b")
